ad8d431a1561e5b231672612992872e061546628
Allan Fontaine <pelagialstudio@outlook.com>
2022-02-09 07:39:25 +0100

    first commit on backend ad files for routes controllers

---
+++ backend/controllers/profile.controllers.js
@@ -0,0 +1,73 @@
+/**
+ * USER CTRL SETTINGS ***********************************************************************************
+ */
+
+/** IMPORT ***********************************************/
+
+/** General import */
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const db_import = require("../config/db_config");
+const db = db_import.DB();
+
+/** EXPORT ***********************************************/
+
+/** userProfile ctrl */
+module.exports.userProfile = async (req, res) => {
+  const { id: idusers } = req.params;
+  const sql = `SELECT * FROM users WHERE idusers = ${idusers}`;
+  db.query(sql, (err, result) => {
+    if (err) {
+      res.status(404).json({ err });
+      throw err;
+    }
+    delete result[0].password;
+    res.status(200).json(result);
+  });
+};
+
+/** updateProfilUser ctrl */
+module.exports.updateProfileUser = async (req, res) => {
+  const { id: idusers } = req.params;
+  const { username, email, password } = req.body;
+
+  const sql = `UPDATE users SET username = '${username}', email = '${email}', password = '${password}' WHERE idusers = ${idusers}`;
+  db.query(sql, (err, result) => {
+    if (err) {
+      res.status(404).json({ err });
+      throw err;
+    }
+    res.status(201).json({ message: "User updated !" });
+  });
+};
+
+/** deleteProfilUser ctrl */
+module.exports.deleteProfileUser = async (req, res) => {
+  const { id: idusers } = req.params;
+  const sql = `DELETE FROM users WHERE idusers = ${idusers}`;
+  db.query(sql, (err, results) => {
+    if (err) {
+      return res.status(404).json({ err });
+    }
+    res.status(200).json("Account deleted");
+  });
+};
+
+/** getAllUser ctrl */
+module.exports.getAllUser = async (req, res) => {
+  try {
+    const sql = 'SELECT `create_time`, `email`, `followers`, `following`, `idusers`, `likes`, `pictures`, `username` FROM users';
+    db.query(sql, (err, result) => {
+      if (!result) {
+        res.status(404).json({ err });
+      } else {
+        res.status(200).json(result);
+      }
+    });
+  }
+  catch (err) {
+    res.status(400).send({ err })
+  }
+};
